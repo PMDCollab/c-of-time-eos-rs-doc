8de37fc3bf7cb8b837a10f7aa379c6b2438da448
theCapypara <theCapypara@users.noreply.github.com>
2022-05-21 16:37:30 +0000

    Documentation update

--- armv5te-none-ndseoseabi-na/doc/implementors/acid_io/io_core/trait.Read.js
+++ armv5te-none-ndseoseabi-na/doc/implementors/acid_io/io_core/trait.Read.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["eos_rs"] = [{"text":"impl <a class=\"trait\" href=\"eos_rs/prelude/trait.Read.html\" title=\"trait eos_rs::prelude::Read\">Read</a> for <a class=\"struct\" href=\"eos_rs/api/io/file/struct.File.html\" title=\"struct eos_rs::api::io::file::File\">File</a>","synthetic":false,"types":["eos_rs::api::io::file::File"]}];
+implementors["eos_rs"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"eos_rs/prelude/trait.Read.html\" title=\"trait eos_rs::prelude::Read\">Read</a> for <a class=\"struct\" href=\"eos_rs/api/io/file/struct.FileReader.html\" title=\"struct eos_rs::api::io::file::FileReader\">FileReader</a>&lt;'a&gt;","synthetic":false,"types":["eos_rs::api::io::file::FileReader"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
